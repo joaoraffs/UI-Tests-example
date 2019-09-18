54889baf75504d69bb94bbe95fd2a7c07773bbd5
Joao Raffs <j.f.raffs@gmail.com>
2019-09-18 10:57:22 -0300

    project debugged and finished

--- BullsEyeTests/BullsEyeTests.swift
+++ BullsEyeTests/BullsEyeTests.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2019 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,38 +26,41 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
+import XCTest
+@testable import BullsEye
 
-class BullsEyeGame {
-  var round = 0
-  let startValue = 50
-  var targetValue = 50
-  var scoreRound = 0
-  var scoreTotal = 0
+class BullsEyeTests: XCTestCase {
   
-  init() {
-    startNewGame()
-  }
-  
-  func startNewGame() {
-    round = 0
-    scoreTotal = 0
-    startNewRound()
-  }
-  
-  func startNewRound() {
-    round = round + 1
-    scoreRound = 0
-    targetValue = 1 + (Int(arc4random()) % 100)
+  var sut: BullsEyeGame!
+  override func setUp() {
+    super.setUp()
+    sut = BullsEyeGame()
+    sut.startNewGame()
   }
 
-  @discardableResult
-  func check(guess: Int) -> Int {
-    let difference = abs(targetValue - guess)
-//    let difference = guess - targetValue
-    scoreRound = 100 - difference
-    scoreTotal = scoreTotal + scoreRound
+  override func tearDown() {
+    sut = nil
+    super.tearDown()
+  }
+  
+  func testScoreIsComputed() {
+    // 1. given
+    let guess = sut.targetValue + 5
     
-    return difference
+    // 2. when
+    sut.check(guess: guess)
+    
+    // 3. then
+    XCTAssertEqual(sut.scoreRound, 95, "Score computed from guess is wrong")
+  }
+  func testScoreIsComputedWhenGuessLTTarget() {
+    // 1. given
+    let guess = sut.targetValue - 5
+    
+    // 2. when
+    sut.check(guess: guess)
+    
+    // 3. then
+    XCTAssertEqual(sut.scoreRound, 95, "Score computed from guess is wrong")
   }
 }
